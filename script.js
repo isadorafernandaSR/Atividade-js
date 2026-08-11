80752969ac8c372a681eb39e3a1b17025bfca775
ISADORA FERNANDA DA SILVA RIFFEL <isadora_riffel@estudante.sesisenai.org.br>
2026-08-10 21:05:36 -0300

    fiz a 6

--- script.js
+++ script.js
@@ -62,7 +62,11 @@ document.getElementById("btn5").addEventListener("click", function (){
 //
 // Dica: leia o valor atual com .textContent, converta para
 // numero com Number() e some 1
-
+document.getElementById("btn6").addEventListener("click", function (){
+    const contador = document.getElementById("contador6")
+    const valorAtual = Number(contador.textContent)
+    contador.textContent = valorAtual + 1
+})
 
 
 // ---------- Exercicio 7 ----------
